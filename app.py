a19b2d7dac67fbeee6dc97aa3fc11c69cfdba14e
Praveen Reddy <praveenreddyg5612@gmail.com>
2025-03-10 15:22:52 +0530

    Fixed static file issue and updated CSS links

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def home():
